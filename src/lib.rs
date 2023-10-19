b4d1ff9552f14a792b5167d5b556e0e1dff00617
Barry Martin <barry.martin@sapphire-digital.com>
2023-10-19 11:42:01 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,22 @@ pub struct Config {
     pub ignore_case: bool,
 }
 
-impl Config{
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+impl Config {
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-      
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
@@ -24,7 +31,7 @@ impl Config{
             file_path,
             ignore_case,
         })
-      }
+    }
 }
 
 
